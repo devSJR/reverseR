a395caae4101c7d7ecd50a8710f4ddcab7cb6c8e
Andrej-Nikolai Spiess, PhD <a.spiess@uke.de>
2017-05-30 16:18:26 +0200

    Update of inflSim.R

--- inflSim.R
+++ inflSim.R
@@ -11,7 +11,7 @@ counter <- function (i)
 n <- 10
 error <- 0.05 #seq(1, 50, by = 5)/100
 slope <- 0.02 #c(0.01, 0.02, 0.05, 0.1)
-reps <-  1:2000
+reps <-  1:100000
 
 GRID <- expand.grid(reps, n, slope, error)
 colnames(GRID) <- c("reps", "n", "slope", "error")
@@ -21,25 +21,21 @@ GRID <- cbind(GRID, MAT)
 isRev <- 0
 
 for (i in 1:nrow(GRID)) {
-  counter(i)
   LME <- lmExact(x = 1:GRID[i, 2], slope = GRID[i, 3], error = GRID[i, 4], 
                  plot = FALSE, seed = i, verbose = FALSE)
   RES <- lmInfl(LME$lm, verbose = FALSE)
   GRID[i, 5] <- RES$origP
   if (!is.null(RES$sel)) {
+    counter(isRev)
     GRID[i, 6:20] <- RES$infl[RES$sel[1], ]
     isRev <- isRev + 1
   }
+  if (isRev == 10000) break
 }
 colnames(GRID)[5:20] <- c("origP", colnames(RES$infl))
+GRID <- GRID[!is.na(GRID$cook.d), ]
 
-print(isRev)
-View(GRID)
-
-z <- scale(as.matrix(GRID))
-z <- z[nrow(z):1, ]
-heatmap(z, Rowv = NA, Colv = NA)
 plot(GRID$cook.d, GRID$dP, pch = 16, cex = 0.5)
-abline(v = 4/n, col = "darkred")
-abline(v = 3 * mean(GRID$cook.d, na.rm = TRUE), col = "darkred")
+abline(v = 4/n, col = "darkred", lwd = 2)
+abline(v = 3 * mean(GRID$cook.d, na.rm = TRUE), col = "darkred", lwd = 2)
 
